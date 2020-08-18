b2cc50ab1841f085bcbdf950a24acae0ffca05ba
Oluwatobi Fatumo <tobyfatumo@gmail.com>
2020-08-18 14:30:14 +0100

    refactoring

--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = require.Router();
-
-router.get('/', (req, res) => { //A call back function here is also called a route handler
-    res.render('index', { title: 'MY Express App', message: 'Hello Word' });
-});
-
-module.exports = router;
